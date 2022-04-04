d56c001d41660103d63c6636fc0311717e1d3540
Jordão Silva <jordao.cassiano@gmail.com>
2022-04-04 12:28:35 -0300

    Update gen_test.py

--- configs/gen_test.py
+++ configs/gen_test.py
@@ -5,7 +5,7 @@ import os
 image_files = []
 for filename in os.listdir("Data_India/val/class1"):
     if filename.endswith(".jpg"):
-        image_files.append("./Data_India/val/class1/" + filename)
+        image_files.append("/content/Data_India/val/class1/" + filename)
         
 with open("configs/test.txt", "w") as outfile:
     for image in image_files:
