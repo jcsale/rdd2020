a00af35d08c1fff1f91b4e04ea7cdc7c64347aab
Jordão Silva <jordao.cassiano@gmail.com>
2022-04-12 12:52:02 -0300

    Update gen_test.py

--- configs/gen_test.py
+++ configs/gen_test.py
@@ -5,9 +5,9 @@ import os
 import os
 
 image_files = []
-for filename in os.listdir("./data/RDD2020-2/valid"):
+for filename in os.listdir("./data/India+Czech-2/valid"):
     if filename.endswith(".jpg"):
-        image_files.append("data/RDD2020-2/valid/" + filename)
+        image_files.append("data/India+Czech-2/valid/" + filename)
         
 with open("configs/test.txt", "w") as outfile:
     for image in image_files:
