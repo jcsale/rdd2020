1cf37794fb9f8628294428c10336ee35c260e077
Jordão Silva <jordao.cassiano@gmail.com>
2022-04-12 12:52:20 -0300

    Update gen_train.py

--- configs/gen_train.py
+++ configs/gen_train.py
@@ -3,9 +3,9 @@
 import os
 
 image_files = []
-for filename in os.listdir("./data/RDD2020-2/train"):
+for filename in os.listdir("./data/India+Czech-2/train"):
     if filename.endswith(".jpg"):
-        image_files.append("data/RDD2020-2/train/" + filename)
+        image_files.append("data/India+Czech-2/train/" + filename)
         
 with open("configs/train.txt", "w") as outfile:
     for image in image_files:
